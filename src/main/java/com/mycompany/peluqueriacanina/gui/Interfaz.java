2eae696ec1ab531f6afeb473d6fe33a2cbad41ac
BrandonAmarillo <brandonxdamarillo@gmail.com>
2026-07-23 19:41:22 -0300

    refactor: Se mejoro el diseño de la UI

--- src/main/java/com/mycompany/peluqueriacanina/gui/Interfaz.java
+++ src/main/java/com/mycompany/peluqueriacanina/gui/Interfaz.java
@@ -9,9 +9,13 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /*
     Interfaz gráfica de la peluquería canina con swing 
@@ -52,132 +56,144 @@ public class Interfaz extends JFrame {
         setTitle("Peluquería Canina");
         setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setLayout(new GridBagLayout());
+        setBackground(Color.GRAY);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5); // Margen entre componentes
-        gbc.anchor = GridBagConstraints.WEST;
+        gbc.anchor = GridBagConstraints.CENTER;
 
-        // Fila 0: NumCliente
+        // Titulo de la peluqueria
+
         gbc.gridx = 0; // Columna 0
         gbc.gridy = 0; // Fila 0
+        gbc.gridwidth = 2; // Ocupa 3 columnas
+        gbc.fill = GridBagConstraints.NONE; // Rellena horizontalmente
+        gbc.anchor = GridBagConstraints.CENTER; // Centra el texto
+        gbc.insets = new Insets(10, 10, 10, 10); // Margen superior e inferior
+        JLabel titulo = new JLabel("Peluquería Canina");
+        titulo.setFont(new Font("Arial", Font.BOLD, 24));
+        titulo.setForeground(Color.BLUE);
+        add(titulo, gbc);
+
+        gbc.anchor = GridBagConstraints.WEST;
+        // Fila 0: NumCliente
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 1; // Fila 1
+        gbc.fill = GridBagConstraints.NONE;
         jlNumCliente = new JLabel("Cliente Nº:");
         add(jlNumCliente, gbc);
 
         gbc.gridx = 1; // Columna 1
-        gbc.gridy = 0; // Fila 0
+        gbc.gridy = 1; // Fila 1
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtNumCliente = new JTextField(20);
         add(jtNumCliente, gbc);
 
         // Fila 1: NombrePerro
         gbc.gridx = 0; // Columna 0
-        gbc.gridy = 1; // Fila 1
+        gbc.gridy = 2; // Fila 2
         gbc.fill = GridBagConstraints.NONE;
         jlNombrePerro = new JLabel("Nombre:");
         add(jlNombrePerro, gbc);
 
         gbc.gridx = 1; // Columna 1
-        gbc.gridy = 1; // Fila 1
+        gbc.gridy = 2; // Fila 2
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtNombrePerro = new JTextField(20);
         add(jtNombrePerro, gbc);
 
-        // Fila 2: Raza 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 2; // Fila 2
+        // Fila 2: Raza
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 3; // Fila 3
         gbc.fill = GridBagConstraints.NONE;
         jlRaza = new JLabel("Raza:");
         add(jlRaza, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtRaza = new JTextField(20);
         add(jtRaza, gbc);
 
-        // Fila 3: Color 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 3; // Fila 3
+        // Fila 3: Color
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 4; // Fila 3
         gbc.fill = GridBagConstraints.NONE;
         jlColor = new JLabel("Color:");
         add(jlColor, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtColor = new JTextField(20);
         add(jtColor, gbc);
 
-        // Fila 4: Alérgico 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 4; // Fila 4
+        // Fila 4: Alérgico
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 5; // Fila 4
         gbc.fill = GridBagConstraints.NONE;
         jlAlergico = new JLabel("Alérgico:");
         add(jlAlergico, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 4;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        jcAlergico = new JComboBox<>();
+        gbc.gridy = 5;
+        jcAlergico = new JComboBox<>(new String[] { "Si", "No" });
         jcAlergico.setEditable(false);
         add(jcAlergico, gbc);
 
-        // Fila 5: Atención Especial 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 5; // Fila 5
+        // Fila 5: Atención Especial
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 6; // Fila 5
         gbc.fill = GridBagConstraints.NONE;
         jlAtencionEspecial = new JLabel("Atención Especial:");
         add(jlAtencionEspecial, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 5;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        jcAtencionEspecial = new JComboBox<>();
+        gbc.gridy = 6;
+        jcAtencionEspecial = new JComboBox<>(new String[] { "Si", "No" });
         jcAtencionEspecial.setEditable(false);
         add(jcAtencionEspecial, gbc);
 
-        // Fila 6: Nombre Dueño 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 6; // Fila 6
+        // Fila 6: Nombre Dueño
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 7; // Fila 6
         gbc.fill = GridBagConstraints.NONE;
         jlNombreDuenio = new JLabel("Nombre Dueño:");
         add(jlNombreDuenio, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 6;
+        gbc.gridy = 7;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtNombreDuenio = new JTextField(20);
         add(jtNombreDuenio, gbc);
 
-        // Fila 7: Cel. Dueño 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 7; // Fila 7
+        // Fila 7: Cel. Dueño
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 8; // Fila 7
         gbc.fill = GridBagConstraints.NONE;
         jlTelefono = new JLabel("Cel. Dueño:");
         add(jlTelefono, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 7;
+        gbc.gridy = 8;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtTelefono = new JTextField(20);
         add(jtTelefono, gbc);
 
-        // Fila 8: Observaciones 
-        gbc.gridx = 0; //Columna 0
-        gbc.gridy = 8; // Fila 8
+        // Fila 8: Observaciones
+        gbc.gridx = 0; // Columna 0
+        gbc.gridy = 9; // Fila 8
         gbc.fill = GridBagConstraints.NONE;
         jlObservaciones = new JLabel("Observaciones:");
         add(jlObservaciones, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 8;
+        gbc.gridy = 9;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         jtObservaciones = new JTextArea(5, 10);
         jtObservaciones.setLineWrap(true);
         add(jtObservaciones, gbc);
-
 
         // Carga y manejo de Imagenes
         try {
@@ -186,7 +202,7 @@ public class Interfaz extends JFrame {
         } catch (Exception e) {
             jlLogo = new JLabel("[Imagen no disponible]", SwingConstants.CENTER);
         }
-        gbc.gridx = 2; // Columna 2 (a la derecha de los campos)
+        gbc.gridx = 5; // Columna 5 (a la derecha de los campos)
         gbc.gridy = 0; // Empieza en la fila 0
         gbc.gridheight = 9; // Ocupa 9 filas (Todos los campos)
         gbc.gridwidth = 1;
@@ -199,6 +215,12 @@ public class Interfaz extends JFrame {
         gbc.gridy = 11;
         gbc.fill = GridBagConstraints.WEST;
         btnLimpiar = new JButton("Limpiar");
+        btnLimpiar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limpiarCampos();
+            }
+        });
         add(btnLimpiar, gbc);
 
         gbc.gridx = 2;
@@ -206,5 +228,18 @@ public class Interfaz extends JFrame {
         gbc.fill = GridBagConstraints.EAST;
         btnGuardar = new JButton("Guardar");
         add(btnGuardar, gbc);
+    }
+
+    // Limpiar todos los campos
+    private void limpiarCampos() {
+        jtNumCliente.setText("");
+        jtNombrePerro.setText("");
+        jtRaza.setText("");
+        jtColor.setText("");
+        jcAlergico.setSelectedIndex(-1);
+        jcAtencionEspecial.setSelectedIndex(-1);
+        jtNombreDuenio.setText("");
+        jtTelefono.setText("");
+        jtObservaciones.setText("");
     }
 }
--- src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
@@ -2,6 +2,136 @@ package com.mycompany.peluqueriacanina.logica;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 public class Mascota implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Column(name = "numero_cliente", unique = true, nullable = false)
+    private int numeroCliente;
+    private String nombre;
+    private String raza;
+    private String color;
+    private String alergico;
+    private String atencionEspecial;
+    private String observaciones;
+
+    @OneToOne
+    private Duenio duenio;
+
+    public Mascota() {
+
+    }
+
+    public Mascota(int id, int numeroCliente, String nombre, String raza, String color, String alergico,
+            String atencionEspecial, String observaciones, Duenio duenio) {
+        this.id = id;
+        this.numeroCliente = numeroCliente;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.alergico = alergico;
+        this.atencionEspecial = atencionEspecial;
+        this.observaciones = observaciones;
+        this.duenio = duenio;
+    }
+
+    public Mascota(int numeroCliente, String nombre, String raza, String color, String alergico,
+            String atencionEspecial, String observaciones, Duenio duenio) {
+        this.numeroCliente = numeroCliente;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.alergico = alergico;
+        this.atencionEspecial = atencionEspecial;
+        this.observaciones = observaciones;
+        this.duenio = duenio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumeroCliente() {
+        return numeroCliente;
+    }
+
+    public void setNumeroCliente(int numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAlergico() {
+        return alergico;
+    }
+
+    public void setAlergico(String alergico) {
+        this.alergico = alergico;
+    }
+
+    public String getAtencionEspecial() {
+        return atencionEspecial;
+    }
+
+    public void setAtencionEspecial(String atencionEspecial) {
+        this.atencionEspecial = atencionEspecial;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Duenio getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(Duenio duenio) {
+        this.duenio = duenio;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota [id=" + id + ", numeroCliente=" + numeroCliente + ", nombre=" + nombre + ", raza=" + raza
+                + ", color=" + color + ", alergico=" + alergico + ", atencionEspecial=" + atencionEspecial
+                + ", observaciones=" + observaciones + ", duenio=" + duenio + "]";
+    }
 
 }
